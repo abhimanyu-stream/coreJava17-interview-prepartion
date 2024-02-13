060ed8b5ed1ac89bb339b4347fc4ae440cd29fc8
Abhimanyu kumar <abhimanyu.intelligence@gmail.com>
2024-02-13 18:27:39 +0530

    first commit

---
+++ src/test/java/com/java8/interview/prepartion/corejava1/programs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java8.interview.prepartion.corejava1.programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
