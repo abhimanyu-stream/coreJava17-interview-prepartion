5b980bb3568ac5791b9abaed8858c8eb2c88e6d3
DESKTOP-J8320O5\acode <stream.abhimanyu@gmail.com>
2024-02-29 08:49:39 +0530

    ok

---
+++ src/main/java/com/java17/interview/prepartion/CountCharacterOccurrenceUsingStreamAPI.java
@@ -0,0 +1,26 @@
+package com.java17.interview.prepartion;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class CountCharacterOccurrenceUsingStreamAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountCharacterOccurrenceUsingStreamAPI.class, args);
+
+
+
+        String str = "HelloworldJavaAAAApplication worlds";
+        System.out.println(Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
+
+    }
+
+
+}
