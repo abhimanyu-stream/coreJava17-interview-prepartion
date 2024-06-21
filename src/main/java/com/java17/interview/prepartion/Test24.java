b1b2b4aba24cd4100c07937eef9afd7ebab3b8db
DESKTOP-J8320O5\acode <stream.abhimanyu@gmail.com>
2024-06-21 13:10:22 +0530

    program added

---
+++ src/main/java/com/java17/interview/prepartion/Test24.java
@@ -0,0 +1,66 @@
+package com.java17.interview.prepartion;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test24 {
+
+    public static void main(String[] args) {
+
+        // Java program to find longest String from Strings in given array
+
+        String[] strArray = {"java","spring boot","microservices","awscloudmicroservicesdevelopment","c"};
+
+        String longest1 = Arrays.stream(strArray)
+                .reduce((s1, s2) -> s1.length() > s2.length() ? s1 : s2)
+                .orElse("");
+
+        System.out.println("Longest string: " + longest1);
+
+
+        List<String> strings = Arrays.asList("cat", "rabbit", "horse", "goat", "rooster", "ooooooooooooooo");
+
+        String longest2 = strings.stream()
+                .max(Comparator.comparingInt(String::length))
+                .get();
+
+        System.out.println("Longest string: " + longest2);
+
+
+
+
+        //List<String> strings = Arrays.asList("cat", "rabbit", "horse", "goat", "rooster", "ooooooooooooooo");
+
+        String longest3 = strings.stream()
+                .collect(Collectors.maxBy(Comparator.comparingInt(String::length)))
+                .orElse("");
+
+        System.out.println("Longest string: " + longest3);
+
+
+
+        // convert first half of String in lowercase and in rest of half in uppercase
+
+        String inputString = "This is a Java Program This is a Java Program";
+        String convertedString = convertHalfCase(inputString);
+        System.out.println("Converted string: " + convertedString);
+
+    }
+    public static String getLongestString(String[] strings) {
+        String longest = "";
+        for (String s : strings) {
+            if (s.length() > longest.length()) {
+                longest = s;
+            }
+        }
+        return longest;
+    }
+    public static String convertHalfCase(String input) {
+        int halfLength = input.length() / 2;
+        String firstHalf = input.substring(0, halfLength).toLowerCase();
+        String secondHalf = input.substring(halfLength).toUpperCase();
+        return firstHalf + secondHalf;
+    }
+}
